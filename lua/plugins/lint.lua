051153c4d1105d1fd367175768bed3ff9415fc29
razvanalex <razvansmadu@gmail.com>
2024-01-14 21:41:31 +0200

    Added partial support for lint toggle
    Linters can polute the buffer. This is not useful when reading other's
    code. By default, keep the linter off. Enable using :LintToggle. Now,
    when toggle back, the LSP output will be also cleared up. In the future,
    we need to enforce the redraw.

--- lua/plugins/lint.lua
+++ lua/plugins/lint.lua
@@ -1,10 +1,26 @@
-require("lint").linters_by_ft = {
+local lint = require("lint")
+lint.linters_by_ft = {
 	markdown = { "vale" },
 	python = { "pylint" },
 }
 
+vim.g.lint = false
+
+vim.api.nvim_create_user_command("LintToggle", function()
+	vim.g.lint = not vim.g.lint
+	if vim.g.lint then
+		lint.try_lint()
+	else
+		vim.diagnostic.reset(nil, 0)
+		-- TODO: redraw the lsp output. right now, it can be redrawn manually
+		-- by entering/exiting insert mode.
+	end
+end, { desc = "Toggle linter" })
+
 vim.api.nvim_create_autocmd({ "BufWritePost" }, {
 	callback = function()
-		require("lint").try_lint()
+		if vim.g.lint then
+			lint.try_lint()
+		end
 	end,
 })
