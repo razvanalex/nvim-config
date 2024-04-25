e5d9918b3cfeab9eeb28b9d4f640a17e987d3742
razvanalex <razvansmadu@gmail.com>
2024-04-26 02:04:50 +0300

    Removed tsserver from ensured installed

--- lua/plugins/lsp/lsp.lua
+++ lua/plugins/lsp/lsp.lua
@@ -294,7 +294,7 @@ return {
 			require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
 
 			require("mason-lspconfig").setup({
-				ensure_installed = { "tsserver", "basedpyright", "lua_ls" },
+				ensure_installed = { "basedpyright", "lua_ls" },
 				handlers = {
 					function(server_name)
 						local server = servers[server_name] or {}
