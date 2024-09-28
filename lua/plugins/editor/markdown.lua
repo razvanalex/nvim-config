955d1394bf16ccaa8525ef3ac2d87f694c6ae15e
razvanalex <razvansmadu@gmail.com>
2024-09-28 21:14:31 +0300

    chore(markdown): Nicer previews in doc hover
    Disable background color on doc hover, the same as before using the
    extension. Use TelescopeNormal for this purpose. It seems NormalFloat
    overwrites syntax highlighting.

--- lua/plugins/editor/markdown.lua
+++ lua/plugins/editor/markdown.lua
@@ -1,7 +1,20 @@
 return {
 	{
 		"MeanderingProgrammer/render-markdown.nvim",
-		opts = {},
 		dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" },
+		file_types = { "markdown", "Avante" },
+		opts = {
+			code = { sign = false },
+			ft = { "markdown", "Avante" },
+			overrides = {
+				buftype = {
+					nofile = {
+						-- Using NormalFloat seems to disable syntax highlighting
+						code = { highlight = "TelescopeNormal" },
+						sign = { enabled = false },
+					},
+				},
+			},
+		},
 	},
 }
