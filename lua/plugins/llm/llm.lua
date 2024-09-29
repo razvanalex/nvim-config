129f225b3e27b46752dcf23bb07c41042646f7cf
razvanalex <razvansmadu@gmail.com>
2024-09-30 00:01:09 +0300

    feat(avante.nvim): Try avante.nvim for LLM integration

--- lua/plugins/llm/llm.lua
+++ lua/plugins/llm/llm.lua
@@ -336,4 +336,118 @@ return {
 			})
 		end,
 	},
+	{
+		"yetone/avante.nvim",
+		event = "VeryLazy",
+		lazy = false,
+		version = false, -- set this if you want to always pull the latest change
+		opts = {
+			provider = "ollama", -- Recommend using Claude
+			auto_suggestions_provider = "ollama", -- Since auto-suggestions are a high-frequency operation and therefore expensive, it is recommended to specify an inexpensive provider or even a free provider: copilot
+			vendors = {
+				ollama = {
+					["local"] = true,
+					endpoint = "http://localhost:11434/v1",
+					model = "llama3.1",
+					parse_curl_args = function(opts, code_opts)
+						return {
+							url = opts.endpoint .. "/chat/completions",
+							headers = {
+								["Accept"] = "application/json",
+								["Content-Type"] = "application/json",
+							},
+							body = {
+								model = opts.model,
+								messages = require("avante.providers").openai.parse_message(code_opts), -- you can make your own message, but this is very advanced
+								max_tokens = 8192,
+								stream = true,
+							},
+						}
+					end,
+					parse_response_data = function(data_stream, event_state, opts)
+						require("avante.providers").openai.parse_response(data_stream, event_state, opts)
+					end,
+				},
+			},
+			behaviour = {
+				auto_suggestions = false, -- Experimental stage
+				auto_set_highlight_group = true,
+				auto_set_keymaps = true,
+				auto_apply_diff_after_generation = false,
+				support_paste_from_clipboard = false,
+			},
+			mappings = {
+				--- @class AvanteConflictMappings
+				diff = {
+					ours = "co",
+					theirs = "ct",
+					all_theirs = "ca",
+					both = "cb",
+					cursor = "cc",
+					next = "]x",
+					prev = "[x",
+				},
+				suggestion = {
+					accept = "<M-l>",
+					next = "<M-]>",
+					prev = "<M-[>",
+					dismiss = "<C-]>",
+				},
+				jump = {
+					next = "]]",
+					prev = "[[",
+				},
+				submit = {
+					normal = "<CR>",
+					insert = "<C-s>",
+				},
+				sidebar = {
+					switch_windows = "<Tab>",
+					reverse_switch_windows = "<S-Tab>",
+				},
+				-- Overwrite defaults
+				ask = "<leader>Aa",
+				edit = "<leader>Ae",
+				refresh = "<leader>Ar",
+				toggle = {
+					default = "<leader>At",
+					debug = "<leader>Ad",
+					hint = "<leader>Ah",
+					suggestion = "<leader>As",
+				},
+			},
+			hints = { enabled = false },
+			windows = {
+				---@type "right" | "left" | "top" | "bottom"
+				position = "right", -- the position of the sidebar
+				wrap = true, -- similar to vim.o.wrap
+				width = 30, -- default % based on available width
+				sidebar_header = {
+					align = "center", -- left, center, right for title
+					rounded = true,
+				},
+			},
+			highlights = {
+				---@type AvanteConflictHighlights
+				diff = {
+					current = "DiffText",
+					incoming = "DiffAdd",
+				},
+			},
+			--- @class AvanteConflictUserConfig
+			diff = {
+				autojump = true,
+				---@type string | fun(): any
+				list_opener = "copen",
+			},
+		},
+		-- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+		build = "make",
+		dependencies = {
+			"nvim-treesitter/nvim-treesitter",
+			"nvim-lua/plenary.nvim",
+			"MunifTanjim/nui.nvim",
+			"nvim-tree/nvim-web-devicons",
+		},
+	},
 }
