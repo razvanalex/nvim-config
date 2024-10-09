5d5cb1230cc91974aa85f47ac479ddb963944d1d
razvanalex <razvansmadu@gmail.com>
2024-10-09 14:44:35 +0300

    feat(editor): Add support for latex features
    This is experimental. Vimtex has lots of features, but right now we care
    more the autocompile feature. Enabling the LSP stuff, makes the
    experience comparable to, if not better than, overleaf. The only thing
    is missing is to directly edit overleaf pages (there is an extension,
    but it needs to be validated).

---
+++ lua/plugins/coding/latex.lua
@@ -0,0 +1,27 @@
+return {
+	{
+		"lervag/vimtex",
+		lazy = false, -- we don't want to lazy load VimTeX
+		-- tag = "v2.15", -- uncomment to pin to a specific release
+		init = function()
+			vim.g.vimtex_view_method = "zathura"
+			vim.g.vimtex_complete_enabled = 0
+			vim.g.vimtex_doc_enabled = 0
+			vim.g.vimtex_fold_enabled = 0
+			vim.g.vimtex_fold_bib_enabled = 0
+			vim.g.vimtex_format_enabled = 0
+			vim.g.vimtex_imaps_enabled = 0
+			vim.g.vimtex_include_search_enabled = 0
+			vim.g.vimtex_indent_enabled = 0
+			vim.g.vimtex_indent_bib_enabled = 0
+			vim.g.vimtex_mappings_enabled = 1
+			vim.g.vimtex_matchparen_enabled = 0
+			vim.g.vimtex_motion_enabled = 0
+			vim.g.vimtex_quickfix_enabled = 0
+			vim.g.vimtex_syntax_enabled = 0
+			vim.g.vimtex_text_obj_enabled = 0
+			vim.g.vimtex_toc_enabled = 1
+			vim.g.vimtex_view_enabled = 1
+		end,
+	},
+}
