e40e65f796c2e64b596c26c2e24e76c23dd9c91f
razvanalex <razvansmadu@gmail.com>
2024-12-01 23:56:40 +0200

    chore(undotree): Persistent undotree
    Hopefully, this makes undotree persistent across neovim sessions

--- lua/plugins/editor/undotree.lua
+++ lua/plugins/editor/undotree.lua
@@ -12,4 +12,15 @@ return { -- fancy undo
 	keys = {
 		{ "<leader>u", vim.cmd.UndotreeToggle, desc = "[U]ndoTree Toggle" },
 	},
+	init = function()
+		if vim.fn.has("persistent_undo") ~= 0 then
+			local target_path = vim.fn.expand("~/.undodir")
+
+			if vim.fn.isdirectory(target_path) == 0 then
+				vim.fn.mkdir(target_path, "p", "0700")
+			end
+			vim.opt.undodir = target_path
+			vim.opt.undofile = true
+		end
+	end,
 }
