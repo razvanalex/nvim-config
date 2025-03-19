5ccc276085b66b8514fbfd988fc1fcd3d1038f51
razvanalex <razvansmadu@gmail.com>
2025-03-19 12:48:14 +0200

    chore: Update keybindings for refactor.nvim
    It seems the API usage changed.

--- lua/plugins/coding/refactoring.lua
+++ lua/plugins/coding/refactoring.lua
@@ -13,62 +13,69 @@ return { -- refactoring
 		{
 			"<leader>ref",
 			function()
-				require("refactoring").refactor("Extract Function")
+				return require("refactoring").refactor("Extract Function")
 			end,
 			desc = "[R]efactor [E]xtract [F]unction",
 			mode = "v",
+			expr = true,
 		},
 		{
 			"<leader>reF",
 			function()
-				require("refactoring").refactor("Extract Function To File")
+				return require("refactoring").refactor("Extract Function To File")
 			end,
 			desc = "[R]efactor [E]xtract Function To [F]ile",
 			mode = "v",
+			expr = true,
 		},
 
 		-- Extract variable supports only visual mode
 		{
 			"<leader>rev",
 			function()
-				require("refactoring").refactor("Extract Variable")
+				return require("refactoring").refactor("Extract Variable")
 			end,
 			desc = "[R]efactor [E]xtract [V]ariable",
 			mode = "v",
+			expr = true,
 		},
 
 		-- Inline func supports only normal
 		{
 			"<leader>rif",
 			function()
-				require("refactoring").refactor("Inline Function")
+				return require("refactoring").refactor("Inline Function")
 			end,
 			desc = "[R]efactor [I]nline [F]unction",
+			expr = true,
 		},
 
 		-- Inline var supports both normal and visual mode
 		{
 			"<leader>riv",
 			function()
-				require("refactoring").refactor("Inline Variable")
+				return require("refactoring").refactor("Inline Variable")
 			end,
 			desc = "[R]efactor [I]nline [V]ariable",
+			expr = true,
 		},
 
 		-- Extract block supports only normal mode
 		{
 			"<leader>reb",
 			function()
-				require("refactoring").refactor("Extract Block")
+				return require("refactoring").refactor("Extract Block")
 			end,
 			desc = "[R]efactor [E]xtract [B]lock",
+			expr = true,
 		},
 		{
 			"<leader>reB",
 			function()
-				require("refactoring").refactor("Extract Block To File")
+				return require("refactoring").refactor("Extract Block To File")
 			end,
 			desc = "[R]efactor [E]xtract Block To [F]ile",
+			expr = true,
 		},
 	},
 }
