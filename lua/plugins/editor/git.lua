99e03ff31d34239245dc2f981c696c059537ff4a
razvanalex <razvansmadu@gmail.com>
2025-05-17 17:38:09 +0300

    chore(git): Simplify gitsigns.nvim plugin
    Also fix an issue that gitsigns attaches to netrw buffers.

--- lua/plugins/editor/git.lua
+++ lua/plugins/editor/git.lua
@@ -48,108 +48,80 @@ return {
 		lazy = true,
 		event = "VeryLazy",
 		cond = not vim.g.vscode,
-		config = function()
-			require("gitsigns").setup({
-				signs = {
-					add = { text = "┃" },
-					change = { text = "┃" },
-					delete = { text = "_" },
-					topdelete = { text = "‾" },
-					changedelete = { text = "~" },
-					untracked = { text = "┆" },
-				},
-				signcolumn = true, -- Toggle with `:Gitsigns toggle_signs`
-				numhl = false, -- Toggle with `:Gitsigns toggle_numhl`
-				linehl = false, -- Toggle with `:Gitsigns toggle_linehl`
-				word_diff = false, -- Toggle with `:Gitsigns toggle_word_diff`
-				watch_gitdir = {
-					follow_files = true,
-				},
-				auto_attach = true,
-				attach_to_untracked = false,
-				current_line_blame = false, -- Toggle with `:Gitsigns toggle_current_line_blame`
-				current_line_blame_opts = {
-					virt_text = true,
-					virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
-					delay = 500,
-					ignore_whitespace = false,
-					virt_text_priority = 100,
-				},
-				current_line_blame_formatter = "<author>, <author_time:%Y-%m-%d> - <summary>",
-				sign_priority = 6,
-				update_debounce = 100,
-				status_formatter = nil, -- Use default
-				max_file_length = 40000, -- Disable if file is longer than this (in lines)
-				preview_config = {
-					-- Options passed to nvim_open_win
-					border = "single",
-					style = "minimal",
-					relative = "cursor",
-					row = 0,
-					col = 1,
-				},
-				on_attach = function(bufnr)
-					if vim.bo[bufnr].filetype == "netrw" then
-						return false
+		opts = {
+			signcolumn = true, -- Toggle with `:Gitsigns toggle_signs`
+			numhl = false, -- Toggle with `:Gitsigns toggle_numhl`
+			linehl = false, -- Toggle with `:Gitsigns toggle_linehl`
+			word_diff = false, -- Toggle with `:Gitsigns toggle_word_diff`
+			current_line_blame = false, -- Toggle with `:Gitsigns toggle_current_line_blame`
+			current_line_blame_opts = {
+				delay = 500,
+			},
+			current_line_blame_formatter = "<author>, <author_time:%Y-%m-%d> - <summary>",
+			preview_config = {
+				border = "single",
+			},
+			on_attach = function(bufnr)
+				if vim.bo[bufnr].filetype == "netrw" then
+					return false
+				end
+
+				local gs = package.loaded.gitsigns
+
+				local function map(mode, l, r, opts)
+					opts = opts or {}
+					opts.buffer = bufnr
+					vim.keymap.set(mode, l, r, opts)
+				end
+
+				-- Navigation
+				map("n", "]c", function()
+					if vim.wo.diff then
+						return "]c"
 					end
+					vim.schedule(function()
+						gs.next_hunk()
+					end)
+					return "<Ignore>"
+				end, { expr = true, desc = "Next Git Hunk" })
 
-					local gs = package.loaded.gitsigns
-
-					local function map(mode, l, r, opts)
-						opts = opts or {}
-						opts.buffer = bufnr
-						vim.keymap.set(mode, l, r, opts)
+				map("n", "[c", function()
+					if vim.wo.diff then
+						return "[c"
 					end
+					vim.schedule(function()
+						gs.prev_hunk()
+					end)
+					return "<Ignore>"
+				end, { expr = true, desc = "Prev Git Hunk" })
 
-					-- Navigation
-					map("n", "]c", function()
-						if vim.wo.diff then
-							return "]c"
-						end
-						vim.schedule(function()
-							gs.next_hunk()
-						end)
-						return "<Ignore>"
-					end, { expr = true, desc = "Next Git Hunk" })
+				-- Actions
+				map("n", "<leader>gs", gs.stage_hunk, { desc = "[G]it Hunk [S]tage" })
+				map("n", "<leader>gr", gs.reset_hunk, { desc = "[G]it Hunk [R]eset" })
+				map("v", "<leader>gs", function()
+					gs.stage_hunk({ vim.fn.line("."), vim.fn.line("v") })
+				end, { desc = "[G]it Hunk [S]tage" })
+				map("v", "<leader>gr", function()
+					gs.reset_hunk({ vim.fn.line("."), vim.fn.line("v") })
+				end, { desc = "[G]it Hunk [R]eset" })
+				map("n", "<leader>gS", gs.stage_buffer, { desc = "[G]it Hunk [S]tage Buffer" })
+				map("n", "<leader>gu", gs.undo_stage_hunk, { desc = "[G]it Hunk [U]ndo Stage" })
+				map("n", "<leader>gR", gs.reset_buffer, { desc = "[G]it [R]eset Buffer" })
+				map("n", "<leader>gp", gs.preview_hunk, { desc = "[G]it [P]review Hunk" })
+				map("n", "<leader>gb", function()
+					gs.blame_line({ full = true })
+				end, { desc = "[G]it Hunk [B]lame" })
+				map("n", "<leader>gtb", gs.toggle_current_line_blame, { desc = "[G]it [T]oggle Line [B]lame" })
+				map("n", "<leader>gd", gs.diffthis, { desc = "[G]it [D]iff This" })
+				map("n", "<leader>gD", function()
+					gs.diffthis("~")
+				end, { desc = "[G]it [D]iff This" })
+				map("n", "<leader>gtd", gs.toggle_deleted, { desc = "[G]it [T]oggle [D]eleted" })
 
-					map("n", "[c", function()
-						if vim.wo.diff then
-							return "[c"
-						end
-						vim.schedule(function()
-							gs.prev_hunk()
-						end)
-						return "<Ignore>"
-					end, { expr = true, desc = "Prev Git Hunk" })
-
-					-- Actions
-					map("n", "<leader>gs", gs.stage_hunk, { desc = "[G]it Hunk [S]tage" })
-					map("n", "<leader>gr", gs.reset_hunk, { desc = "[G]it Hunk [R]eset" })
-					map("v", "<leader>gs", function()
-						gs.stage_hunk({ vim.fn.line("."), vim.fn.line("v") })
-					end, { desc = "[G]it Hunk [S]tage" })
-					map("v", "<leader>gr", function()
-						gs.reset_hunk({ vim.fn.line("."), vim.fn.line("v") })
-					end, { desc = "[G]it Hunk [R]eset" })
-					map("n", "<leader>gS", gs.stage_buffer, { desc = "[G]it Hunk [S]tage Buffer" })
-					map("n", "<leader>gu", gs.undo_stage_hunk, { desc = "[G]it Hunk [U]ndo Stage" })
-					map("n", "<leader>gR", gs.reset_buffer, { desc = "[G]it [R]eset Buffer" })
-					map("n", "<leader>gp", gs.preview_hunk, { desc = "[G]it [P]review Hunk" })
-					map("n", "<leader>gb", function()
-						gs.blame_line({ full = true })
-					end, { desc = "[G]it Hunk [B]lame" })
-					map("n", "<leader>gtb", gs.toggle_current_line_blame, { desc = "[G]it [T]oggle Line [B]lame" })
-					map("n", "<leader>gd", gs.diffthis, { desc = "[G]it [D]iff This" })
-					map("n", "<leader>gD", function()
-						gs.diffthis("~")
-					end, { desc = "[G]it [D]iff This" })
-					map("n", "<leader>gtd", gs.toggle_deleted, { desc = "[G]it [T]oggle [D]eleted" })
-
-					-- Text object
-					map({ "o", "x" }, "gh", ":<C-U>Gitsigns select_hunk<CR>", { desc = "[G]it Select [H]unk" })
-				end,
-			})
-		end,
+				-- Text object
+				map({ "o", "x" }, "gh", ":<C-U>Gitsigns select_hunk<CR>", { desc = "[G]it Select [H]unk" })
+			end,
+		},
 	},
 	{
 		"pwntester/octo.nvim",
