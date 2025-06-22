8d3262764ea645eb0e8638583ab60b794f3a6f0a
razvanalex <razvansmadu@gmail.com>
2025-06-22 14:44:24 +0300

    chore(theme): Reorganize theme

--- lua/plugins/ui/theme.lua
+++ lua/plugins/ui/theme.lua
@@ -1,3 +1,19 @@
+---Make transparent background for some highlight groups
+---@param common table<string, any> Common highlight groups to overwrite
+---@param C table<string, any> Colors from the colorscheme
+---@return table<string, any> Overwritten highlight groups
+local function fix_transparency(common, C)
+	if os.getenv("TRANSPARENT") == "true" then
+		local transparent_overwrites = {
+			NormalFloat = { fg = C.text, bg = C.mantle }, -- Normal text in floating windows.
+		}
+		for k, v in pairs(transparent_overwrites) do
+			common[k] = v
+		end
+	end
+	return common
+end
+
 return {
 	{ -- vscode color theme
 		"Mofiqul/vscode.nvim",
@@ -40,43 +56,64 @@ return {
 				gitsigns = true,
 				neotest = true,
 			},
-			custom_highlights = function(C)
-				local common = {
-					-- Highlight groups for removed lines
-					DiffviewDiffDelete = { bg = "none", fg = "#3B4252" },
+			highlight_overrides = {
+				latte = function(latte)
+					return fix_transparency({
+						-- Highlight groups for removed lines
+						DiffviewDiffDelete = { bg = "none", fg = "#D8DEE9" },
 
-					-- Highlight groups for delete
-					DiffviewDelete = { bg = "#301a1e" },
-					DiffviewDeleteText = { bg = "#7f2f2f" },
+						-- Highlight groups for delete
+						DiffviewDelete = { bg = "#f7d8db" },
+						DiffviewDeleteText = { bg = "#f0b6ba" },
 
-					-- Highlight groups for right panel
-					DiffviewAdd = { bg = "#12261d" },
-					DiffviewAddText = { bg = "#1c562b" },
+						-- Highlight groups for right panel
+						DiffviewAdd = { bg = "#a4cdb7" },
+						DiffviewAddText = { bg = "#85c1a7" },
 
-					-- Git signs
-					GitSignsAdd = { fg = "#6a9955" },
-					GitSignsChange = { fg = "#0078d4" },
-					GitSignsDelete = { fg = "#f85149" },
+						-- Git signs
+						GitSignsAdd = { fg = "#2da44e" },
+						GitSignsChange = { fg = "#0969da" },
+						GitSignsDelete = { fg = "#cf222e" },
 
-					-- Vimdiff
-					DiffAdd = { bg = "#12261d" },
-					DiffDelete = { bg = "#301a1e" },
-					DiffChange = { bg = "#1f2742" },
-					DiffText = { bg = "#2d385d" },
+						-- Vimdiff
+						DiffAdd = { bg = "#a4cdb7" },
+						DiffDelete = { bg = "#f7d8db" },
+						DiffChange = { bg = "#e9f1ff" },
+						DiffText = { bg = "#d1dfff" },
 
-					FloatBorder = { fg = C.mantle, bg = C.mantle },
-					CmpNormal = { bg = C.surface0 },
-				}
-				if os.getenv("TRANSPARENT") == "true" then
-					local transparent_overwrites = {
-						NormalFloat = { fg = C.text, bg = C.mantle }, -- Normal text in floating windows.
-					}
-					for k, v in pairs(transparent_overwrites) do
-						common[k] = v
-					end
-				end
-				return common
-			end,
+						FloatBorder = { fg = latte.mantle, bg = latte.mantle },
+						CmpNormal = { bg = latte.surface0 },
+					}, latte)
+				end,
+				mocha = function(mocha)
+					return fix_transparency({
+						-- Highlight groups for removed lines
+						DiffviewDiffDelete = { bg = "none", fg = "#3B4252" },
+
+						-- Highlight groups for delete
+						DiffviewDelete = { bg = "#301a1e" },
+						DiffviewDeleteText = { bg = "#7f2f2f" },
+
+						-- Highlight groups for right panel
+						DiffviewAdd = { bg = "#12261d" },
+						DiffviewAddText = { bg = "#1c562b" },
+
+						-- Git signs
+						GitSignsAdd = { fg = "#6a9955" },
+						GitSignsChange = { fg = "#0078d4" },
+						GitSignsDelete = { fg = "#f85149" },
+
+						-- Vimdiff
+						DiffAdd = { bg = "#12261d" },
+						DiffDelete = { bg = "#301a1e" },
+						DiffChange = { bg = "#1f2742" },
+						DiffText = { bg = "#2d385d" },
+
+						FloatBorder = { fg = mocha.mantle, bg = mocha.mantle },
+						CmpNormal = { bg = mocha.surface0 },
+					}, mocha)
+				end,
+			},
 		},
 		keys = {
 			{
