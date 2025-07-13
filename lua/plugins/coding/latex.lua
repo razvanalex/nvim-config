08549e1bbdaefd02f33048247902e6eafb406d62
razvanalex <razvansmadu@gmail.com>
2025-07-13 23:51:50 +0300

    chore(latex): Use either zathura or skimpdf for latex preview
    Depending on which pdf viewer is install, use it. zathura is not
    available on macos, therefore use skimpdf.

--- lua/plugins/coding/latex.lua
+++ lua/plugins/coding/latex.lua
@@ -6,7 +6,34 @@ return {
 		ft = { "latex", "tex" },
 		-- tag = "v2.15", -- uncomment to pin to a specific release
 		init = function()
-			vim.g.vimtex_view_method = "zathura"
+			local function is_executable(cmd)
+				return vim.fn.executable(cmd) == 1
+			end
+
+			if is_executable("zathura") then
+				vim.g.vimtex_view_method = "zathura"
+			elseif is_executable("skimpdf") then
+				vim.g.vimtex_view_method = "skim"
+				vim.g.vimtex_view_skim_sync = 1
+				vim.g.vimtex_view_skim_activate = 1
+
+				-- Function to focus back to Vim/terminal from Skim
+				vim.api.nvim_create_autocmd("User", {
+					pattern = "VimtexEventViewReverse",
+					callback = function()
+						vim.fn.system("open -a kitty")
+						vim.cmd("redraw!")
+					end,
+					desc = "Focus back to terminal when using backward search from Skim",
+				})
+			else
+				vim.notify(
+					"Neither Zathura nor Skim is installed. Please install one of them for PDF viewing.",
+					vim.log.levels.WARN
+				)
+				vim.g.vimtex_view_enabled = 0
+			end
+
 			vim.g.vimtex_complete_enabled = 0
 			vim.g.vimtex_doc_enabled = 0
 			vim.g.vimtex_fold_enabled = 0
