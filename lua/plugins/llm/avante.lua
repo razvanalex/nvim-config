0f61ba39d4ef8797a4fc51b554b81a6f142781bf
razvanalex <razvansmadu@gmail.com>
2025-07-13 23:53:26 +0300

    chore(avante): Use by default gpt4.1
    GitHub Copilot changed the pricing model. GPT4.1 is included in the Pro
    tier. Anthropic models cost 1 credit, and there are 300 credits per
    month. The model can be easily changed if needed.

--- lua/plugins/llm/avante.lua
+++ lua/plugins/llm/avante.lua
@@ -42,12 +42,14 @@ return {
 		providers = {
 			copilot = {
 				endpoint = "https://api.githubcopilot.com",
-				model = "claude-3.7-sonnet",
+				model = "gpt-4.1",
 				proxy = nil, -- [protocol://]host[:port] Use this proxy
 				allow_insecure = false, -- Allow insecure server connections
 				timeout = 30000, -- Timeout in milliseconds
-				temperature = 0,
-				max_tokens = 20480,
+				extra_request_body = {
+					temperature = 0,
+					max_tokens = 64000,
+				},
 			},
 		},
 		behaviour = {
