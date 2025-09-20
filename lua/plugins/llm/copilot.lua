d6664b13bac2a4bbc9238bb1bba59730fabaf1b2
razvanalex <razvansmadu@gmail.com>
2025-09-20 21:54:07 +0300

    feat: Add NES support from copilot.lua

--- lua/plugins/llm/copilot.lua
+++ lua/plugins/llm/copilot.lua
@@ -1,26 +1,39 @@
 return {
 	{
 		"zbirenbaum/copilot.lua",
+		dependencies = {
+			"copilotlsp-nvim/copilot-lsp",
+			init = function()
+				vim.g.copilot_nes_debounce = 500
+			end,
+		},
 		cmd = "Copilot",
 		event = "InsertEnter",
 		enabled = not vim.g.vscode,
-		config = function()
-			require("copilot").setup({
-				suggestion = {
-					enabled = true,
-					auto_trigger = true,
-					hide_during_completion = false,
-					debounce = 75,
-					keymap = {
-						accept = false,
-						accept_word = false,
-						accept_line = "<M-y>",
-						next = "<M-]>",
-						prev = "<M-[>",
-						dismiss = "<C-]>",
-					},
+		opts = {
+			nes = {
+				enabled = true,
+				auto_trigger = true,
+				keymap = {
+					accept_and_goto = "<Tab>",
+					accept = "<S-Tab",
+					dismiss = "<Esc>",
 				},
-			})
-		end,
+			},
+			suggestion = {
+				enabled = true,
+				auto_trigger = true,
+				hide_during_completion = false,
+				debounce = 75,
+				keymap = {
+					accept = "<M-Y>",
+					accept_word = false,
+					accept_line = "<M-y>",
+					next = "<M-]>",
+					prev = "<M-[>",
+					dismiss = "<C-]>",
+				},
+			},
+		},
 	},
 }
