d56e97a0155c47ce8b583bf3beba00fca8efaa8d
razvanalex <razvansmadu@gmail.com>
2025-12-28 13:51:00 +0200

    feat: Add iron.nvim for REPL

---
+++ lua/plugins/editor/iron.lua
@@ -0,0 +1,69 @@
+return {
+	"Vigemus/iron.nvim",
+	event = "VeryLazy",
+	lazy = true,
+	cmd = {
+		"IronRepl",
+		"IronReplHere",
+		"IronRestart",
+		"IronSend",
+		"IronFocus",
+		"IronHide",
+		"IronWatch",
+		"IronAttach",
+	},
+	keys = {
+		{ "<space>Rr", "<cmd>IronRepl<cr>", desc = "Open REPL" },
+		{ "<space>Rh", "<cmd>IronReplHere<cr>", desc = "Open REPL here" },
+		{ "<space>RR", "<cmd>IronRestart<cr>", desc = "Restart REPL" },
+		{ "<space>Rf", "<cmd>IronFocus<cr>", desc = "Focus REPL" },
+		{ "<space>Ri", "<cmd>IronHide<cr>", desc = "Hide REPL" },
+	},
+	opts = function()
+		local common = require("iron.fts.common")
+		local view = require("iron.view")
+
+		return {
+			config = {
+				scratch_repl = true,
+				repl_definition = {
+					sh = {
+						command = { "zsh" },
+					},
+					python = {
+						command = { "ipython", "--no-autoindent" },
+						format = common.bracketed_paste_python,
+						block_dividers = { "# %%", "#%%" },
+						env = { PYTHON_BASIC_REPL = "1" },
+					},
+				},
+				repl_filetype = function(bufnr, ft)
+					return ft
+				end,
+				dap_integration = true,
+				repl_open_cmd = view.right(40),
+			},
+			keymaps = {
+				send_motion = "<space>Rsc",
+				visual_send = "<space>Rsc",
+				send_file = "<space>Rsf",
+				send_line = "<space>Rsl",
+				send_until_cursor = "<space>Rsu",
+				send_mark = "<space>Rsm",
+				send_code_block = "<space>Rsb",
+				send_code_block_and_move = "<space>Rsn",
+				mark_motion = "<space>Rmc",
+				mark_visual = "<space>Rmc",
+				remove_mark = "<space>Rmd",
+				cr = "<space>Rs<cr>",
+				interrupt = "<space>Rs<space>",
+				exit = "<space>Rsq",
+				clear = "<space>Rcl",
+			},
+			highlight = {
+				italic = true,
+			},
+			ignore_blank_lines = true,
+		}
+	end,
+}
