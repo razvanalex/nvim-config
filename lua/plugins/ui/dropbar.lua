138e6f1526458c34d3cd115c9c8ce2c84810751a
razvanalex <razvansmadu@gmail.com>
2026-05-20 11:40:28 +0300

    chore: update dropbar.nvim for nvim 0.13
    This should be a temporary fix

--- lua/plugins/ui/dropbar.lua
+++ lua/plugins/ui/dropbar.lua
@@ -5,10 +5,30 @@ return {
 		"nvim-telescope/telescope-fzf-native.nvim",
 		build = "make",
 	},
-	config = function()
+	opts = {
+		bar = {
+			update_events = {
+				buf = {
+					"FileChangedShellPost",
+					"TextChanged",
+					"ModeChanged",
+				},
+			},
+		},
+	},
+	config = function(_, opts)
+		require("dropbar").setup(opts)
 		local dropbar_api = require("dropbar.api")
 		vim.keymap.set("n", "<Leader>;", dropbar_api.pick, { desc = "Pick symbols in winbar" })
 		vim.keymap.set("n", "[;", dropbar_api.goto_context_start, { desc = "Go to start of current context" })
 		vim.keymap.set("n", "];", dropbar_api.select_next_context, { desc = "Select next context" })
+
+		-- Manual fix for BufModifiedSet deprecation in Neovim 0.13
+		vim.api.nvim_create_autocmd("OptionSet", {
+			pattern = "modified",
+			callback = function()
+				require("dropbar.utils.bar").exec("update", { buf = vim.api.nvim_get_current_buf() })
+			end,
+		})
 	end,
 }
